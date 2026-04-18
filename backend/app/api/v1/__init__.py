2624e81a93d40c737b8da85df99d6f3c1d723f03
vans1r <serebrogml@gmail.com>
2026-04-18 21:58:32 +0300

    create users router + connect to api/v1 router

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .tasks import router as tasks_router
+from .users import router as users_router
 
 router = APIRouter(prefix="/v1")
 
 router.include_router(tasks_router)
+router.include_router(users_router)
---
+++ backend/app/api/v1/users.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from core.database.db_helper import db_helper
+from schemas.users import UserCreate, UserUpdatePartial
+from crud import users as users_crud
+
+router = APIRouter(tags=["Users"], prefix="/users")
+
+
+@router.post("")
+async def create_user(
+    user: UserCreate,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    return await users_crud.create_user(user=user, session=session)
+
+
+@router.get("")
+async def get_users(
+    name: str | None = None,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    if name:
+        return await users_crud.get_users_by_name(name=name, session=session)
+    return await users_crud.get_users(session=session)
+
+
+@router.patch("/{id}")
+async def update_user(
+    id: int,
+    new_user_data: UserUpdatePartial,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    user = await users_crud.get_user_by_id(id=id, session=session)
+    if user:
+        return await users_crud.update_user_partial(
+            user=user, new_user_data=new_user_data, session=session
+        )
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+
+@router.delete("/{id}")
+async def delete_user(
+    id: int,
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    user = await users_crud.get_user_by_id(id=id, session=session)
+    if user:
+        await users_crud.delete_user(user=user, session=session)
+        return {"status": "success"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
