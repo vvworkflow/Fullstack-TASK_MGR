5e2d4112b82b3942baca5ec90f449f9b2451c498
vans1r <serebrogml@gmail.com>
2026-04-19 16:02:21 +0300

    create schemas for statistics

--- backend/app/schemas/tasks_changelogs.py
+++ backend/app/schemas/tasks_changelogs.py
@@ -1,5 +1,20 @@
+from typing import Sequence
+
 from pydantic import BaseModel
 
 
-class AvgTimeInStatus(BaseModel):
-    pass
+class StatusAvgTimeRead(BaseModel):
+    status: str
+    avg_seconds: int
+
+
+class TopUserRead(BaseModel):
+    user_id: int
+    username: str
+    tasks_done: int
+    avg_seconds_to_done: int
+
+
+class StatisticsRead(BaseModel):
+    avg_time_per_status: Sequence[StatusAvgTimeRead]
+    top_users: Sequence[TopUserRead]
