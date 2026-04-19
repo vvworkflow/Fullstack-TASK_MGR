8d3c9488e02f7a002abdf77ba4b75011fed1362c
vans1r <serebrogml@gmail.com>
2026-04-19 16:05:11 +0300

    create statistics_router + connect to __all__

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from .tasks import router as tasks_router
 from .users import router as users_router
+from .statistics import router as statistics_router
 
 router = APIRouter(prefix="/v1")
 
 router.include_router(tasks_router)
 router.include_router(users_router)
+router.include_router(statistics_router)
---
+++ backend/app/api/v1/statistics.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.database.db_helper import db_helper
+from crud import tasks_changelogs as tasks_changelogs_crud
+from crud import users as users_crud
+
+router = APIRouter(tags=["Statistics"], prefix="/statistics")
+
+
+@router.get("/avg-time-per-status")
+async def get_avg_time_per_status(
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    return await tasks_changelogs_crud.get_avg_time_per_status(session=session)
+
+
+@router.get("/top-productive-users")
+async def get_top_productive_users(
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    return await users_crud.get_top_productive_users(session=session)
