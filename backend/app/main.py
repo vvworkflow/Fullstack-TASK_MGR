e639dce0f45475a620ccbf811979ac4d5187e675
vans1r <serebrogml@gmail.com>
2026-04-19 22:20:25 +0300

    fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,8 +11,7 @@ app = FastAPI(title="API for local task manager")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://localhost:3000",
-        "http://localhost:5173",
+        "http://localhost:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
