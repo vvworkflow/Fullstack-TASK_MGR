1a447cd780610aeec7152b5111eff438b399a118
vans1r <serebrogml@gmail.com>
2026-04-19 16:05:55 +0300

    create crud get_top_productive_users() in users_crud

--- backend/app/crud/users.py
+++ backend/app/crud/users.py
@@ -1,8 +1,9 @@
 from typing import Sequence
 
-from sqlalchemy import select
+from sqlalchemy import select, func, text, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from models import TaskChangelog, Task
 from models.users import User
 from schemas.users import UserCreate, UserUpdatePartial
 
@@ -47,3 +48,53 @@ async def update_user_partial(
 async def delete_user(user: User, session: AsyncSession) -> None:
     await session.delete(user)
     await session.commit()
+
+
+async def get_top_productive_users(session: AsyncSession) -> list[dict]:
+    # avg(done_time - task_created_at) по пользователю
+    stmt = (
+        select(
+            User.id.label("user_id"),
+            User.name.label("username"),
+            func.count(TaskChangelog.id).label("tasks_done"),
+            func.avg(
+                func.timestampdiff(
+                    text("SECOND"),
+                    Task.created_at,
+                    TaskChangelog.created_at,
+                )
+            ).label("avg_seconds_to_done"),
+        )
+        .join(Task, Task.id == TaskChangelog.task_id)
+        .join(User, User.id == TaskChangelog.changed_by_id)
+        .where(
+            and_(
+                TaskChangelog.field == "status",
+                TaskChangelog.new_value == "done",
+            )
+        )
+        .group_by(User.id, User.name)
+        .order_by(
+            func.avg(
+                func.timestampdiff(
+                    text("SECOND"),
+                    Task.created_at,
+                    TaskChangelog.created_at,
+                )
+            ).asc()
+        )
+        .limit(3)
+    )
+
+    result = await session.execute(stmt)
+    rows = result.mappings().all()
+
+    return [
+        {
+            "user_id": row["user_id"],
+            "username": row["username"],
+            "tasks_done": row["tasks_done"],
+            "avg_seconds_to_done": int(row["avg_seconds_to_done"] or 0),
+        }
+        for row in rows
+    ]
