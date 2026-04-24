dca390e1c2361074ff957c51430d1b27c8bc015a
vans1r <serebrogml@gmail.com>
2026-04-24 17:01:10 +0300

    added new statuses and new priority in enums/tasks.py

--- backend/app/enums/tasks.py
+++ backend/app/enums/tasks.py
@@ -4,10 +4,13 @@ from enum import Enum
 class TaskStatus(str, Enum):
     backlog = "BACKLOG"
     in_progress = "IN_PROGRESS"
+    review = "REVIEW"
     done = "DONE"
+    archive = "ARCHIVE"
 
 
 class TaskPriority(str, Enum):
+    critical = "CRITICAL"
     high = "HIGH"
     medium = "MEDIUM"
     low = "LOW"
