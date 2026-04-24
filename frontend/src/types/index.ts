768628baa137008e658b4ccc288b200e111b8e3a
vans1r <serebrogml@gmail.com>
2026-04-24 13:43:39 +0300

    create types/index.ts for project interface types(like pydantic schemas)

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,37 @@
+export type TaskStatus = 'BACKLOG' | 'IN_PROGRESS' | 'REVIEW' | 'DONE'
+export type TaskPriority = 'LOW' | 'MEDIUM' | 'HIGH' | 'CRITICAL'
+export type UserRole = 'admin' | 'manager' | 'developer'
+
+export interface User {
+    id: number
+    username: string
+    fullname: string
+    role: UserRole
+}
+
+export interface Task {
+    id: number
+    title: string
+    description: string | null
+    status: TaskStatus
+    priority: TaskPriority
+    created_by_id: number | null
+    assignee_id: number | null
+}
+
+export interface TaskCreatePayload {
+    title: string
+    description: string | null
+    status: TaskStatus
+    priority: TaskPriority
+    created_by_id: number
+    assignee_id: number | null
+}
+
+export interface TaskUpdatePayload {
+    title?: string
+    description?: string | null
+    status?: TaskStatus
+    priority?: TaskPriority
+    assignee_id?: number | null
+}
