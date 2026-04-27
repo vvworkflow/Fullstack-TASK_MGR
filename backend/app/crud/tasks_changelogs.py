7c170d07b79f57d2806bba0f092d0d106c2e34ae
vans1r <serebrogml@gmail.com>
2026-04-28 00:54:29 +0300

    added order_by in task.status statistic

--- backend/app/crud/tasks_changelogs.py
+++ backend/app/crud/tasks_changelogs.py
@@ -1,8 +1,16 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.tasks_changelogs import TaskChangelog
-from sqlalchemy import select, func, text
+from sqlalchemy import select, func, text, case
 from models.tasks import Task
+
+status_order = {
+    "BACKLOG": 1,
+    "IN_PROGRESS": 2,
+    "REVIEW": 3,
+    "DONE": 4,
+    "ARCHIVE": 5,
+}
 
 
 async def create_changelog(
@@ -47,12 +55,16 @@ async def get_avg_time_per_status(session: AsyncSession) -> list[dict]:
         inner.c.task_created_at,
     )
 
-    stmt = select(
-        inner.c.old_value.label("status"),
-        func.avg(
-            func.timestampdiff(text("SECOND"), entry_time, inner.c.created_at)
-        ).label("avg_seconds"),
-    ).group_by(inner.c.old_value)
+    stmt = (
+        select(
+            inner.c.old_value.label("status"),
+            func.avg(
+                func.timestampdiff(text("SECOND"), entry_time, inner.c.created_at)
+            ).label("avg_seconds"),
+        )
+        .group_by(inner.c.old_value)
+        .order_by(case(status_order, value=inner.c.old_value, else_=99))
+    )
 
     result = await session.execute(stmt)
     rows = result.mappings().all()
