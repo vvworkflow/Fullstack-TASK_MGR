66df72297a29f105a901ed61a82f2426f62cde62
vans1r <serebrogml@gmail.com>
2026-04-28 00:57:46 +0300

    updated User schemes using fastapi_users schemas

--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -1,31 +1,23 @@
 from datetime import datetime
-
-from pydantic import BaseModel
-
 from enums.users import UserRole
+from fastapi_users import schemas
+from custom_types.user_id import UserIDType
 
 
-class UserBase(BaseModel):
-    pass
-
-
-class UserCreate(BaseModel):
+class UserCreate(schemas.BaseUserCreate):
     username: str
     fullname: str
-    password: str
     role: UserRole
 
 
-class UserRead(BaseModel):
-    id: int
+class UserRead(schemas.BaseUser[UserIDType]):
     username: str
     fullname: str
     role: UserRole
     created_at: datetime
 
 
-class UserUpdatePartial(BaseModel):
+class UserUpdatePartial(schemas.BaseUserUpdate):
     username: str | None = None
     fullname: str | None = None
-    password: str | None = None
     role: UserRole | None = None
