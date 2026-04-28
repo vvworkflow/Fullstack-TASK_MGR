f83e4b1fde23b96b5e2d6ed2c8c2c0d8d021feb7
vans1r <serebrogml@gmail.com>
2026-04-29 00:26:03 +0300

    updated user router: delete all and connect fastapi_users user routers

--- backend/app/api/v1/users.py
+++ backend/app/api/v1/users.py
@@ -1,20 +1,16 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette import status
 
+from api.v1.auth import fastapi_users
+from core.config import settings
 from core.database.db_helper import db_helper
-from schemas.users import UserCreate, UserUpdatePartial
+from schemas.users import UserUpdatePartial, UserRead
 from crud import users as users_crud
 
-router = APIRouter(tags=["Users"], prefix="/users")
-
-
-@router.post("")
-async def create_user(
-    user: UserCreate,
-    session: AsyncSession = Depends(db_helper.session_getter),
-):
-    return await users_crud.create_user(user_data=user, session=session)
+router = APIRouter(
+    tags=["Users"],
+    prefix=settings.api.v1.users,
+)
 
 
 @router.get("")
@@ -27,30 +23,6 @@ async def get_users(
     return await users_crud.get_users(session=session)
 
 
-@router.patch("/{id}")
-async def update_user(
-    id: int,
-    new_user_data: UserUpdatePartial,
-    session: AsyncSession = Depends(db_helper.session_getter),
-):
-    user = await users_crud.get_user_by_id(id=id, session=session)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-    return await users_crud.update_user_partial(
-        user=user, new_user_data=new_user_data, session=session
-    )
-
-
-@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(
-    id: int,
-    session: AsyncSession = Depends(db_helper.session_getter),
-):
-    user = await users_crud.get_user_by_id(id=id, session=session)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-    await users_crud.delete_user(user=user, session=session)
+router.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdatePartial),
+)
